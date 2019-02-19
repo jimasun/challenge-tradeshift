fe446678d78e80c2f6d4db38729e58fdc2ccfe50
Marius Jigoreanu <marius.jigoreanu@gmail.com>
2019-02-19 17:10:52 +0100

    Improved tests
    * added more tests
    * improved teh validation in `typeOfTriangle`

--- spec/modules/Triangle.spec.js
+++ spec/modules/Triangle.spec.js
@@ -1,17 +1,30 @@
-import test from 'ava'
-import {
-  isValidSideValue
-} from '../../src/modules/triangle'
+import * as Triangle from '../../src/modules/Triangle'
 
-test('should reject invalid values', t => {
-  t.falsy(isValidSideValue(NaN).isValid, 'NaN')
-  t.falsy(isValidSideValue({}).isValid, 'object')
-  t.falsy(isValidSideValue(0).isValid, '0')
-  t.falsy(isValidSideValue(-1).isValid, 'negative')
-  t.falsy(isValidSideValue('1').isValid, 'string')
+const test = require('ninos')(require('ava'))
+
+test('isValidSideValue(int s)', t => {
+  t.false(Triangle.isValidSideValue(NaN).isValid, 'NaN')
+  t.false(Triangle.isValidSideValue({}).isValid, 'object')
+  t.false(Triangle.isValidSideValue(0).isValid, 'zero')
+  t.false(Triangle.isValidSideValue(-1).isValid, 'negative')
+  t.false(Triangle.isValidSideValue('1').isValid, 'string')
+  t.true(Triangle.isValidSideValue(1).isValid, 'integer')
+  t.true(Triangle.isValidSideValue(1.345).isValid, 'fractional')
 })
 
-test('should accept valid values', t => {
-  t.truthy(isValidSideValue(1).isValid, 'integer')
-  t.truthy(isValidSideValue(1.345).isValid, 'fractional')
+test('isValidTriangle([a, b, c])', t => {
+  const s = t.context.spy(Triangle, 'isValidSideValue') // NOT WORKING
+  t.false(Triangle.isValidTriangle([1, 4, 2]).isValid, 'a+c<b [1, 4, 2]')
+  t.is(s.calls.length, 0, '3xisValidSideValue')
+  t.true(Triangle.isValidTriangle([2, 3, 2]).isValid, 'a+c<b [2, 3, 2]')
+  t.is(s.calls.length, 0, '3xisValidSideValue')
+  t.throws(() => { Triangle.isValidTriangle([-1, 4, 2]) }, Error, 'throw new Error')
+})
+
+test('typeOfTriangle([a, b, c])', t => {
+  t.is(Triangle.typeOfTriangle([1, 1, 1]).type, 'equilateral', 'equilateral')
+  t.is(Triangle.typeOfTriangle([2, 1.5, 1.5]).type, 'isoscel', 'isoscel')
+  t.is(Triangle.typeOfTriangle([2.5, 4, 2.5]).type, 'isoscel', 'isoscel')
+  t.is(Triangle.typeOfTriangle([7, 2, 6]).type, 'scalane', 'scalane')
+  t.throws(() => { Triangle.typeOfTriangle([1, 4, 2]) }, Error, 'throw new Error')
 })
--- src/modules/triangle.js
+++ src/modules/triangle.js
@@ -1,6 +1,64 @@
+const isValidSideValue = (side) => {
+  if (isNaN(side)) return {
+    msg: `The value should be a number. NaN provided`,
+    isValid: false,
+    value: side
+  }
+
+  if (typeof side !== 'number') return {
+    msg: `The value should be a number. (${typeof side}) ${side} provided`,
+    isValid: false,
+    value: side
+  }
+
+  if (side <= 0) return {
+    msg: 'The value should be greater than 0',
+    isValid: false,
+    value: side
+  }
+
+  return {
+    msg: 'The value is a valid side',
+    isValid: true,
+    value: side
+  }
+}
+
+const isValidTriangle = ([a, b, c]) => {
+  if (
+    !isValidSideValue(a).isValid ||
+    !isValidSideValue(b).isValid ||
+    !isValidSideValue(c).isValid
+  ) {
+    throw new Error('one of the sides not valid. Use isValidSideValue(Number: s)')
+  }
+
+  const max = Math.max(a, b, c)
+
+  if (a === max && b + c <= max) return {
+    isValid: false,
+    msg: '`!(b + c < a)`: the sum of the two smaller sides should be smaller than the greater'
+  }
+
+  if (b === max && a + c <= max) return {
+    isValid: false,
+    msg: '`!(a + c < b)`: the sum of the two smaller sides should be smaller than the greater'
+  }
+
+  if (c === max && a + b <= max) return {
+    isValid: false,
+    msg: '`!(a + b < c)`: the sum of the two smaller sides should be smaller than the greater'
+  }
+
+  return {
+    isValid: true,
+    msg: `triangle [${a}, ${b}, ${c}] is valid`
+  }
+}
+
 const typeOfTriangle = ([a, b, c]) => {
-  if (!isValidSideValue(a).isValid || !isValidSideValue(b).isValid || !isValidSideValue(c).isValid) {
-    throw new Error('invalid value(s) provided. use `isValidSideValue(int a)`')
+  if (!isValidTriangle([a, b, c]).isValid) {
+    throw new Error('invalid triangle value(s) provided. use isValidTriangle([a, b, c])')
   }
 
   const triangle = [a, b, c]
@@ -37,66 +95,8 @@ const triangleCongruentSides = ([a, b, c]) => {
   return 'a != b != c'
 }
 
-const isValidSideValue = (side) => {
-  if (isNaN(side)) return {
-    msg: `The value should be a number. NaN provided`,
-    isValid: false,
-    value: side
-  }
-
-  if (typeof side !== 'number') return {
-    msg: `The value should be a number. (${typeof side}) ${side} provided`,
-    isValid: false,
-    value: side
-  }
-
-  if (side <= 0) return {
-    msg: 'The value should be greater than 0',
-    isValid: false,
-    value: side
-  }
-
-  return {
-    msg: 'The value is a valid side',
-    isValid: true,
-    value: side
-  }
-}
-
-const isValidTriangle = ([a, b, c]) => {
-  if (
-    !isValidSideValue(a).isValid ||
-    !isValidSideValue(b).isValid ||
-    !isValidSideValue(c).isValid
-  ) {
-    throw new Error('one of the sides not valid. Use isValidSideValue((number) s)')
-  }
-
-  const max = Math.max(a, b, c)
-
-  if (a === max && b + c <= max) return {
-    isValid: false,
-    msg: '`!(b + c < a)`: the sum of the two smaller sides should be smaller than the greater'
-  }
-
-  if (b === max && a + c <= max) return {
-    isValid: false,
-    msg: '`!(a + c < b)`: the sum of the two smaller sides should be smaller than the greater'
-  }
-
-  if (c === max && a + b <= max) return {
-    isValid: false,
-    msg: '`!(a + b < c)`: the sum of the two smaller sides should be smaller than the greater'
-  }
-
-  return {
-    isValid: true,
-    msg: `triangle [${a}, ${b}, ${c}] is valid`
-  }
-}
-
 export {
-  typeOfTriangle,
   isValidSideValue,
-  isValidTriangle
+  isValidTriangle,
+  typeOfTriangle
 }
